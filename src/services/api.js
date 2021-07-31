1af78dc9e945250b3df174b072488fcb54fb6dfb
ManoelPatrocinio <47128362+ManoelPatrocinio@users.noreply.github.com>
2021-07-31 01:39:09 -0300

    :up: variavel REACT_APP_API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 require("dotenv").config(); //para poder ler as variaveis do .env
-const api_url = process.env.REACT_APP_API_URL;
+const api_url = process.env.REACT_APP_API;
 const api = axios.create({
   baseURL: api_url,
 });
